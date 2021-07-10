8c8a1aaa608bc843b130080cff107197093c20b0
hkpark97 <77339221+hkpark97@users.noreply.github.com>
2021-07-10 13:58:50 -0400

    added header and footer

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -3,14 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './partials/header/header.component';
-import { FooterComponent } from './partials/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    FooterComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
