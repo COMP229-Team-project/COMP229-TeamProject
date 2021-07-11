d0c7edf6b279064b9c3608faf1ec4066ae3239b8
Kenny <kenpfowler@gmail.com>
2021-07-11 11:35:53 -0400

    modified survey-preview component to be dynamic

--- Client/src/app/partials/survey-preview/survey-preview.component.ts
+++ Client/src/app/partials/survey-preview/survey-preview.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'survey-preview',
@@ -9,6 +9,11 @@ import { Component, OnInit } from '@angular/core';
   ],
 })
 export class SurveyPreviewComponent implements OnInit {
+  @Input() post = {
+    title: '',
+    description: '',
+  };
+
   constructor() {}
 
   ngOnInit(): void {}
