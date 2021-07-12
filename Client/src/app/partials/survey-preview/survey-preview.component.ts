381ea91ae768732701f5ba1f0189616dae9b11b9
Kenny <kenpfowler@gmail.com>
2021-07-12 12:30:46 -0400

    added dynamic image to quiz preview card

--- Client/src/app/partials/survey-preview/survey-preview.component.ts
+++ Client/src/app/partials/survey-preview/survey-preview.component.ts
@@ -3,16 +3,14 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'survey-preview',
   templateUrl: './survey-preview.component.html',
-  styleUrls: [
-    './survey-preview.component.css',
-    '../../../../node_modules/@angular/material/prebuilt-themes/indigo-pink.css',
-  ],
+  styleUrls: ['./survey-preview.component.css'],
 })
 export class SurveyPreviewComponent implements OnInit {
   //input to accept post objects from a service
   @Input() post = {
     title: '',
     description: '',
+    imgURL: '',
   };
 
   constructor() {}
