251e5a493d05439799364e9e61419918e4f2e228
Ken <development@kennyf.ca>
2021-07-13 09:39:42 -0400

    built html template view and tested controller for survey

--- Client/src/app/survey-builder-form/survey-builder-form.component.ts
+++ Client/src/app/survey-builder-form/survey-builder-form.component.ts
@@ -10,15 +10,44 @@ export class SurveyBuilderFormComponent implements OnInit {
   isLinear = false;
   firstFormGroup!: FormGroup;
   secondFormGroup!: FormGroup;
+  thirdFormGroup!: FormGroup;
 
   constructor(public _formBuilder: FormBuilder) {}
 
+  //// TODO: Create group elements for each question
+  // Find a way to accept data and post to Database
   ngOnInit() {
     this.firstFormGroup = this._formBuilder.group({
-      firstCtrl: ['', Validators.required],
+      title: ['', Validators.required],
+      description: ['', Validators.required],
+      avatarURL: ['', Validators.required],
     });
     this.secondFormGroup = this._formBuilder.group({
-      secondCtrl: ['', Validators.required],
+      question1: ['', Validators.required],
+      answer1: ['', Validators.required],
+      answer2: ['', Validators.required],
+      answer3: ['', Validators.required],
+      answer4: ['', Validators.required],
+    });
+    this.thirdFormGroup = this._formBuilder.group({
+      question1: ['', Validators.required],
+      answer1: ['', Validators.required],
+      answer2: ['', Validators.required],
+      answer3: ['', Validators.required],
+      answer4: ['', Validators.required],
+    });
+
+    //observables to log form data being inputted
+    this.firstFormGroup.valueChanges.subscribe((value) => {
+      console.log(value);
+    });
+    //observables to log form data being inputted
+    this.secondFormGroup.valueChanges.subscribe((value) => {
+      console.log(value);
+    });
+    //observables to log form data being inputted
+    this.thirdFormGroup.valueChanges.subscribe((value) => {
+      console.log(value);
     });
   }
 }
