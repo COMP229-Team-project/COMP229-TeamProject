15071c8e246b23ec5864a0f42e6cf888d18cf0cc
Ken <development@kennyf.ca>
2021-07-15 15:07:45 -0400

    pointed application toward our team database

--- server/config/db.js
+++ server/config/db.js
@@ -1,16 +1,7 @@
-/*
-
- @file
- server/config/db.js
-  Hyekyeong Park(Kate) || 301148613 || COMP229 || Midterm 
-
-*/
-
-module.exports = {
-  //local MongoDB deployment ->
-  //  "URI": "mongodb://localhost/books229"
-
-  URI: "mongodb+srv://KatePark:LwGkIq0IOQreGnu9@dbserver.7pj1d.mongodb.net/BookStore?retryWrites=true&w=majority",
-  PROJECTURI:
-    "mongodb+srv://root1:roor123@mongodbserver.9wne7.mongodb.net/SurveyData?retryWrites=true&w=majority",
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.DB = void 0;
+exports.DB = {
+    URI: "mongodb+srv://root1:roor123@mongodbserver.9wne7.mongodb.net/SurveyData?retryWrites=true&w=majority",
 };
+//# sourceMappingURL=db.js.map
---
+++ server/config/db.ts
@@ -0,0 +1,11 @@
+/*
+
+ @file
+ server/config/db.js
+  Hyekyeong Park(Kate) || 301148613 || COMP229 || Midterm 
+
+*/
+
+export const DB = {
+  URI: "mongodb+srv://root1:roor123@mongodbserver.9wne7.mongodb.net/SurveyData?retryWrites=true&w=majority",
+};
