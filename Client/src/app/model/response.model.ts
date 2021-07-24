7fd42b907ae866eb824897f193ba8c83133facd9
Ken <development@kennyf.ca>
2021-07-24 18:54:51 -0400

    created data model for submissions

---
+++ Client/src/app/model/response.model.ts
@@ -0,0 +1,9 @@
+export class surveyResponse {
+  constructor(
+    public id: string,
+    public response1: string,
+    public response2: string,
+    public response3: string,
+    public response4: string
+  ) {}
+}
