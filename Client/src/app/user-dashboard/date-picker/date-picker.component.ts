c1f1f63900693de022edec6fca9395e0bf7fef4e
Ken <development@kennyf.ca>
2021-07-27 01:26:40 -0400

    created component to specifically update the date from the dashboard

---
+++ Client/src/app/user-dashboard/date-picker/date-picker.component.ts
@@ -0,0 +1,37 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { RestDataSource } from 'src/app/model/rest.datasource';
+
+@Component({
+  selector: 'date-picker',
+  templateUrl: './date-picker.component.html',
+  styleUrls: ['./date-picker.component.css'],
+})
+export class DatePickerComponent implements OnInit {
+  @Input() dateRange = {
+    id: String,
+    startDate: Date,
+    endDate: Date,
+  };
+
+  firstFormGroup!: FormGroup;
+
+  constructor(public fb: FormBuilder, private restDataSource: RestDataSource) {}
+
+  ngOnInit() {
+    this.firstFormGroup = this.fb.group({
+      startDate: [this.dateRange.startDate],
+      endDate: [this.dateRange.endDate],
+    });
+  }
+
+  UpdateActiveDateRange(): void {
+    let updateData = {
+      id: this.dateRange.id,
+      startDate: this.firstFormGroup.value.startDate,
+      endDate: this.firstFormGroup.value.endDate,
+    };
+    console.log(updateData);
+    this.restDataSource.UpdateActiveDateRange(updateData);
+  }
+}
