653ec8728d560c0e706ba60d489ab4a27b388f52
Ken <development@kennyf.ca>
2021-07-28 11:44:23 -0400

    moved materail components into shared module to organize app module and support code splitting

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -1,58 +1,42 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
-import { SurveyComponent } from './pages/survey/survey.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-//import angular material support
-import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule } from '@angular/material/button';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatHeaderComponent } from './partials/mat-header/mat-header.component';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatListModule } from '@angular/material/list';
-
 //import components to display active surveys
-import { SurveyListComponent } from './partials/survey-list/survey-list.component';
-import { SurveyPreviewComponent } from './partials/survey-preview/survey-preview.component';
-import { MatFooterComponent } from './partials/mat-footer/mat-footer.component';
+import { SurveyListComponent } from './survey-preview/survey-list/survey-list.component';
+import { SurveyPreviewComponent } from './survey-preview/survey-preview.component';
 
 //import components for building survey builder
 import { SurveyBuilderFormComponent } from './survey-builder-form/survey-builder-form.component';
-import { MatStepperModule } from '@angular/material/stepper';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatDatepickerModule } from '@angular/material/datepicker';
 
-//import material components to impliment response collection modal
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatRadioModule } from '@angular/material/radio';
 import {
   ResponseCollectionModalComponent,
   ResponseCollectionDialog,
 } from './response-collection/response-collection-modal/response-collection-modal.component';
 
-//import the module that communicates with the backend
-//GETS survey JSON data
-import { SurveyModule } from './model/model.module';
-import { MatNativeDateModule } from '@angular/material/core';
-import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
-
 //Module to build user dashboard
-import { MatTableModule } from '@angular/material/table';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { DatePickerComponent } from './user-dashboard/date-picker/date-picker.component';
-import { ShellComponent } from './shell/shell.component';
-import { MatExpansionModule } from '@angular/material/expansion';
+import {
+  JwtModule,
+  JwtHelperService,
+  JwtInterceptor,
+} from '@auth0/angular-jwt';
+import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { SurveyComponent } from './pages/survey/survey.component';
+import { SharedModule } from './shared/shared.module';
+import { SurveyModule } from './model/model.module';
+import { AuthService } from './model/auth.service';
+
+export function jwtTokenGetter(): string | null {
+  return localStorage.getItem('id_token');
+}
 
 @NgModule({
   declarations: [
@@ -60,45 +44,29 @@ import { MatExpansionModule } from '@angular/material/expansion';
     HomeComponent,
     AboutComponent,
     ContactComponent,
-    SurveyComponent,
     SurveyPreviewComponent,
     SurveyListComponent,
-    MatHeaderComponent,
-    MatFooterComponent,
     SurveyBuilderFormComponent,
     ResponseCollectionModalComponent,
     ResponseCollectionDialog,
+    DatePickerComponent,
     UserDashboardComponent,
     DashboardComponent,
-    DatePickerComponent,
-    ShellComponent,
+    SurveyComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    ReactiveFormsModule,
-    MatButtonModule,
-    MatCardModule,
-    MatGridListModule,
-    MatToolbarModule,
-    MatMenuModule,
-    MatIconModule,
-    MatStepperModule,
-    MatFormFieldModule,
-    MatInputModule,
     SurveyModule,
-    MatDialogModule,
-    MatRadioModule,
-    MatDatepickerModule,
-    FormsModule,
-    MatNativeDateModule,
-    MatTableModule,
-    MatSidenavModule,
-    MatListModule,
-    MatExpansionModule,
+    SharedModule,
+    JwtModule.forRoot({
+      config: {
+        tokenGetter: jwtTokenGetter,
+      },
+    }),
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ Client/src/app/shared/shared.module.ts
@@ -0,0 +1,55 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+//import angular material support
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatStepperModule } from '@angular/material/stepper';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatTableModule } from '@angular/material/table';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { ShellComponent } from './shell/shell.component';
+import { RouterModule } from '@angular/router';
+
+const components = [ShellComponent];
+const modules = [
+  CommonModule,
+  ReactiveFormsModule,
+  MatButtonModule,
+  MatCardModule,
+  MatGridListModule,
+  MatToolbarModule,
+  MatMenuModule,
+  MatIconModule,
+  MatStepperModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatDialogModule,
+  MatRadioModule,
+  MatDatepickerModule,
+  FormsModule,
+  MatNativeDateModule,
+  MatTableModule,
+  MatSidenavModule,
+  MatListModule,
+  MatExpansionModule,
+  RouterModule,
+];
+@NgModule({
+  declarations: [...components],
+  imports: [...modules],
+  exports: [...components, ...modules],
+})
+export class SharedModule {}
