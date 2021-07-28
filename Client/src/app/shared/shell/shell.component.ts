ff93e81ea83550a38e6f4978f122a5c3366d550d
Ken <development@kennyf.ca>
2021-07-28 17:38:05 -0400

    added login and logout features to app shell

--- Client/src/app/shared/shell/shell.component.ts
+++ Client/src/app/shared/shell/shell.component.ts
@@ -1,15 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
 import { AuthService } from 'src/app/model/auth.service';
+import { User } from 'src/app/model/user.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'shell',
   templateUrl: './shell.component.html',
   styleUrls: ['./shell.component.css'],
 })
-export class ShellComponent {
+export class ShellComponent implements OnInit {
+  user!: User;
   isHandset$: Observable<boolean> = this.breakpointObserver
     .observe([Breakpoints.Handset])
     .pipe(
@@ -19,10 +22,25 @@ export class ShellComponent {
 
   constructor(
     private breakpointObserver: BreakpointObserver,
-    private auth: AuthService
+    private auth: AuthService,
+    private router: Router
   ) {}
 
+  ngOnInit() {
+    this.user = new User();
+  }
+
+  isLoggedIn(): boolean {
+    const result = this.auth.authenticated;
+    if (result) {
+      this.user = JSON.parse(localStorage.getItem('user')!);
+    }
+    return result;
+  }
+
   logout(): void {
-    this.auth.logout();
+    this.auth.logout().subscribe((data) => {
+      this.router.navigate(['login']);
+    });
   }
 }
