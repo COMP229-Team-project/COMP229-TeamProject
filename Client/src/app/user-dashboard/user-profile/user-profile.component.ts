03f56d82a1f5d5d7764e900796be8dd637845dac
Ken <development@kennyf.ca>
2021-08-04 12:18:48 -0400

    created component to display user profile information

---
+++ Client/src/app/user-dashboard/user-profile/user-profile.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { RestDataSource } from 'src/app/model/rest.datasource';
+import { User } from 'src/app/model/user.model';
+
+@Component({
+  selector: 'user-profile',
+  templateUrl: './user-profile.component.html',
+  styleUrls: ['./user-profile.component.css'],
+})
+export class UserProfileComponent implements OnInit {
+  form!: FormGroup;
+  user!: User;
+
+  constructor(
+    private fb: FormBuilder,
+    private restDataSource: RestDataSource
+  ) {}
+
+  ngOnInit(): void {
+    this.user = JSON.parse(localStorage.getItem('user')!);
+    this.form = this.fb.group({
+      firstName: [this.user.firstName, [Validators.required]],
+      lastName: [this.user.lastName, [Validators.required]],
+      email: [this.user.email, [Validators.required, Validators.email]],
+    });
+  }
+
+  get email() {
+    return this.form.get('email');
+  }
+
+  get firstName() {
+    return this.form.get('firstName');
+  }
+
+  get lastName() {
+    return this.form.get('lastName');
+  }
+}
