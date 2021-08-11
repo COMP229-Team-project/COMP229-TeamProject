f1a165e698522b738b93de1a7e4cd5dbeb81d29e
Ken <development@kennyf.ca>
2021-08-10 21:53:57 -0400

    removed refernces to unused components and updated emojis with icons

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AboutComponent } from './pages/about/about.component';
-import { ContactComponent } from './pages/contact/contact.component';
 import { HomeComponent } from './pages/home/home.component';
 import { SurveyComponent } from './pages/survey/survey.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
@@ -9,7 +7,6 @@ import { AuthGuard } from './gaurds/auth.guard';
 //app routing module defines routes for the application
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
-  { path: 'about', component: AboutComponent },
   {
     path: 'dashboard',
     component: DashboardComponent,
@@ -17,7 +14,6 @@ const routes: Routes = [
   },
   { path: 'survey', component: SurveyComponent, canActivate: [AuthGuard] },
   { path: 'survey/:id', component: SurveyComponent, canActivate: [AuthGuard] },
-  { path: 'contact', component: ContactComponent },
   {
     path: 'login',
     loadChildren: () =>
