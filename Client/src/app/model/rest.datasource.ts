c02e223d6a8155570bb94d8e9a46d19a6df1c427
Ken <development@kennyf.ca>
2021-08-16 17:00:17 -0400

    switched rest client to production

--- Client/src/app/model/rest.datasource.ts
+++ Client/src/app/model/rest.datasource.ts
@@ -36,27 +36,27 @@ export class RestDataSource implements OnInit {
   //get an observable array of surveys from our api
   getSurveys(): Observable<Survey[]> {
     //PRODUCTION
-    // return this.http.get<Survey[]>('/api');
+    return this.http.get<Survey[]>('/api');
     //LOCAL
-    return this.http.get<Survey[]>(this.baseURL + '/api');
+    // return this.http.get<Survey[]>(this.baseURL + '/api');
   }
 
   getUserSurveys(): Observable<Survey[]> {
     this.loadToken();
     let creatorId = JSON.parse(localStorage.getItem('user')!).id;
     //PRODUCTION
-    // return this.http.post<Survey[]>(
-    //   '/api/usersurveys',
-    //   { creatorId: creatorId },
-    //   this.httpOptions
-    // );
-
-    //LOCAL
     return this.http.post<Survey[]>(
-      this.baseURL + '/api/usersurveys',
+      '/api/usersurveys',
       { creatorId: creatorId },
       this.httpOptions
     );
+
+    //LOCAL
+    // return this.http.post<Survey[]>(
+    //   this.baseURL + '/api/usersurveys',
+    //   { creatorId: creatorId },
+    //   this.httpOptions
+    // );
   }
 
   //post a survey to the api for processing
@@ -65,39 +65,28 @@ export class RestDataSource implements OnInit {
     survey.creatorId = JSON.parse(localStorage.getItem('user')!).id;
 
     //PRODUCTION
-    // this.http
-    //   .post('/api/add', survey, this.httpOptions)
-    //   .toPromise()
-    //   .then((response) => {
-    //     this.router.navigate(['home']);
-    //   });
-
-    //LOCAL
     this.http
-      .post(this.baseURL + '/api/add', survey, this.httpOptions)
+      .post('/api/add', survey, this.httpOptions)
       .toPromise()
       .then((response) => {
         this.router.navigate(['home']);
       });
+
+    //LOCAL
+    // this.http
+    //   .post(this.baseURL + '/api/add', survey, this.httpOptions)
+    //   .toPromise()
+    //   .then((response) => {
+    //     this.router.navigate(['home']);
+    //   });
   }
 
   //delete a specific survey from the database via the api
   DeleteSurvey(id: string): void {
     this.loadToken();
     //PRODUCTION
-    // this.http
-    //   .delete('/api/delete/' + id, this.httpOptions)
-    //   .toPromise()
-    //   .then((response) => {
-    //     location.reload();
-    //   })
-    //   .catch((error) => {
-    //     console.error(error);
-    //   });
-
-    //LOCAL
     this.http
-      .delete(this.baseURL + '/api/delete/' + id, this.httpOptions)
+      .delete('/api/delete/' + id, this.httpOptions)
       .toPromise()
       .then((response) => {
         location.reload();
@@ -105,71 +94,71 @@ export class RestDataSource implements OnInit {
       .catch((error) => {
         console.error(error);
       });
+
+    //LOCAL
+    // this.http
+    //   .delete(this.baseURL + '/api/delete/' + id, this.httpOptions)
+    //   .toPromise()
+    //   .then((response) => {
+    //     location.reload();
+    //   })
+    //   .catch((error) => {
+    //     console.error(error);
+    //   });
   }
 
   //get a specific survey from the database
   GetSurveyToEdit(id: string): Observable<any> {
     this.loadToken();
     //PRODUCTION
-    // return this.http.get('/api/edit/' + id, this.httpOptions);
+    return this.http.get('/api/edit/' + id, this.httpOptions);
     //LOCAL
-    return this.http.get(this.baseURL + '/api/edit/' + id, this.httpOptions);
+    // return this.http.get(this.baseURL + '/api/edit/' + id, this.httpOptions);
   }
 
   //update the values of a spcific survey
   EditSurvey(id: string, survey: EditableSurvey): void {
     this.loadToken();
     //PRODUCTION
-    // this.http
-    //   .post('/api/edit/' + id, survey, this.httpOptions)
-    //   .toPromise()
-    //   .then((response) => {
-    //     this.router.navigate(['home']);
-    //   });
-
-    //LOCAL
     this.http
-      .post(this.baseURL + '/api/edit/' + id, survey, this.httpOptions)
+      .post('/api/edit/' + id, survey, this.httpOptions)
       .toPromise()
       .then((response) => {
         this.router.navigate(['home']);
       });
+
+    //LOCAL
+    // this.http
+    //   .post(this.baseURL + '/api/edit/' + id, survey, this.httpOptions)
+    //   .toPromise()
+    //   .then((response) => {
+    //     this.router.navigate(['home']);
+    //   });
   }
 
   AddResponse(response: surveyResponse): void {
     //PRODUCTION
-    // this.http
-    //   .post('/api/responses', response)
-    //   .toPromise()
-    //   .then((response) => {
-    //     this.router.navigate(['home']);
-    //   });
-
-    //LOCAL
     this.http
-      .post(this.baseURL + '/api/responses', response)
+      .post('/api/responses', response)
       .toPromise()
       .then((response) => {
         this.router.navigate(['home']);
       });
+
+    //LOCAL
+    // this.http
+    //   .post(this.baseURL + '/api/responses', response)
+    //   .toPromise()
+    //   .then((response) => {
+    //     this.router.navigate(['home']);
+    //   });
   }
 
   UpdateActiveDateRange(dateRange: any): void {
     this.loadToken();
     //PRODUCTION
-    // this.http
-    //   .post('/api/updatedaterange', dateRange, this.httpOptions)
-    //   .toPromise()
-    //   .then((response) => {
-    //     location.reload();
-    //   })
-    //   .catch((error) => {
-    //     console.error(error);
-    //   });
-    //LOCAL
-
     this.http
-      .post(this.baseURL + '/api/updatedaterange', dateRange, this.httpOptions)
+      .post('/api/updatedaterange', dateRange, this.httpOptions)
       .toPromise()
       .then((response) => {
         location.reload();
@@ -177,28 +166,32 @@ export class RestDataSource implements OnInit {
       .catch((error) => {
         console.error(error);
       });
-  }
 
-  EmailSurveyDataToUser(survey: Survey): Observable<any> {
-    this.loadToken();
-    //PRODUCTION
-    // return this.http
-    //   .post('/api/emailsurveydatatouser', this.httpOptions)
+    //LOCAL
+    // this.http
+    //   .post(this.baseURL + '/api/updatedaterange', dateRange, this.httpOptions)
     //   .toPromise()
     //   .then((response) => {
-    //     return response;
+    //     location.reload();
     //   })
     //   .catch((error) => {
     //     console.error(error);
     //   });
+  }
 
+  EmailSurveyDataToUser(survey: Survey): Observable<any> {
+    this.loadToken();
     let data = { survey: survey, user: this.user };
+
+    //PRODUCTION
+    return this.http.post('/api/emailsurveydatatouser', data, this.httpOptions);
+
     //LOCAL
-    return this.http.post(
-      this.baseURL + '/api/emailsurveydatatouser',
-      data,
-      this.httpOptions
-    );
+    // return this.http.post(
+    //   this.baseURL + '/api/emailsurveydatatouser',
+    //   data,
+    //   this.httpOptions
+    // );
   }
 
   //////////////////////////////////////////////////////////////////////////
@@ -206,23 +199,8 @@ export class RestDataSource implements OnInit {
   UpdateUserProfile(updatedProfile: any): void {
     this.loadToken();
     //PRODUCTION
-    // this.http
-    //   .post('/api/updatedaterange', dateRange, this.httpOptions)
-    //   .toPromise()
-    //   .then((response) => {
-    //     location.reload();
-    //   })
-    //   .catch((error) => {
-    //     console.error(error);
-    //   });
-
-    //LOCAL
     this.http
-      .post(
-        this.baseURL + '/api/updateuserprofile',
-        updatedProfile,
-        this.httpOptions
-      )
+      .post('/api/updateuserprofile', updatedProfile, this.httpOptions)
       .toPromise()
       .then((response) => {
         let creatorId = JSON.parse(localStorage.getItem('user')!).id;
@@ -233,29 +211,47 @@ export class RestDataSource implements OnInit {
       .catch((error) => {
         console.error(error);
       });
+
+    //LOCAL
+    // this.http
+    //   .post(
+    //     this.baseURL + '/api/updateuserprofile',
+    //     updatedProfile,
+    //     this.httpOptions
+    //   )
+    //   .toPromise()
+    //   .then((response) => {
+    //     let creatorId = JSON.parse(localStorage.getItem('user')!).id;
+    //     updatedProfile.id = creatorId;
+    //     localStorage.setItem('user', JSON.stringify(updatedProfile));
+    //     location.reload();
+    //   })
+    //   .catch((error) => {
+    //     console.error(error);
+    //   });
   }
 
   //authenticate a usre by posting to the API's login route with a user object and the headers set to httpOptions
   register(user: User): Observable<any> {
     //PRODUCTION
-    // return this.http.post<any>('/api/register', user, this.httpOptions);
+    return this.http.post<any>('/api/register', user, this.httpOptions);
     //LOCAL
-    return this.http.post<any>(
-      this.baseURL + '/api/register',
-      user,
-      this.httpOptions
-    );
+    // return this.http.post<any>(
+    //   this.baseURL + '/api/register',
+    //   user,
+    //   this.httpOptions
+    // );
   }
 
   authenticate(user: User): Observable<any> {
     //PRODUCTION
-    // return this.http.post<any>('/api/login', user, this.httpOptions);
+    return this.http.post<any>('/api/login', user, this.httpOptions);
     //LOCAL
-    return this.http.post<any>(
-      this.baseURL + '/api/login',
-      user,
-      this.httpOptions
-    );
+    // return this.http.post<any>(
+    //   this.baseURL + '/api/login',
+    //   user,
+    //   this.httpOptions
+    // );
   }
 
   //store user data within the properties of this service and in local storage
@@ -273,9 +269,9 @@ export class RestDataSource implements OnInit {
     this.user = null;
     localStorage.clear();
     //PRODUCTION
-    // return this.http.get<any>('/api/logout', this.httpOptions);
+    return this.http.get<any>('/api/logout', this.httpOptions);
     //LOCAL
-    return this.http.get<any>(this.baseURL + '/api/logout', this.httpOptions);
+    // return this.http.get<any>(this.baseURL + '/api/logout', this.httpOptions);
   }
 
   //check if the user
